72acf9f9f6d70a5efeb975ad85cb68efb6b27c3a
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-27 17:08:54 -0700

    begin new main

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Setting the game screen size
+
+// Rendering the board
+
+// Starting a new Minesweeper on click
+
+// Updating the board on each move
+
+// Handling when the game is won and lost
